17b166b6dbd619bf04dbd70b41aebfb0a4f324e0
leoneves <leoneves@leoneves-Inspiron-1440.(none)>
2013-10-27 00:09:32 -0200

    commit with first code

---
+++ src/main/java/br/uva/seguroDevida/IndexController.java
@@ -0,0 +1,24 @@
+package br.uva.seguroDevida;
+
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+import br.uva.seguroDevida.util.Util;
+
+@Resource
+public class IndexController {
+
+    private final Result result;
+    private Util util;
+
+    public IndexController(Result result, Util util) {
+        this.result = result;
+        this.util = util;
+    }
+
+    @Path("/")
+    public void index() {
+        result.include("", "VRaptor!");
+    }
+
+}
---
+++ src/main/java/br/uva/seguroDevida/util/Util.java
@@ -0,0 +1,10 @@
+package br.uva.seguroDevida.util;
+
+import br.com.caelum.vraptor.Resource;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+@Resource
+public class Util {
+
+}
